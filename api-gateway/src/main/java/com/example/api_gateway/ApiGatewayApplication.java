a9f154f1d0611a5a2b3b0f162f0ba87b75fe3730
kirubak492 <128248690+kirubak492@users.noreply.github.com>
2025-01-09 19:22:29 +0530

    Add files via upload
    Api-Gateway file is added

---
+++ api-gateway/src/main/java/com/example/api_gateway/ApiGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.example.api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
